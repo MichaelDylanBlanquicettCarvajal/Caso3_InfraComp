0c4e15036fde8bc14067d6210ed1d84d66e50fb8
Paola <78177442+Paolaaaaaa@users.noreply.github.com>
2022-11-06 19:33:27 -0500

    calculo gy

--- seguridad20222_servidor/src/seguridad20222_servidor/Client.java
+++ seguridad20222_servidor/src/seguridad20222_servidor/Client.java
@@ -8,6 +8,7 @@ import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.net.Socket;
 import java.security.PublicKey;
+import java.security.SecureRandom;
 
 public class Client {
     public static final int PUERTO =4030;
@@ -25,21 +26,26 @@ public class Client {
         
     }
 
-    public void procesar(BufferedReader stdln, BufferedReader pIn, PrintWriter pout) throws IOException
+    public void procesar(BufferedReader stdln, BufferedReader pIn, PrintWriter pout) throws Exception
     {
+        boolean ck = false;
+
+
+
+
         System.out.println("Escriba un numero ");
         String secure_int =  stdln.readLine();
-        pout.println(secure_int);// envío secure Int
+        pout.println(secure_int);// * envío secure Int
 
         this.publicaServer = sc.read_kplus("datos_asim_srv.pub",pIn.readLine());
 
 
-        this.g= pIn.readLine();// llegada de G
+        this.g= pIn.readLine();// * llegada de G
         System.out.println("Esta es tu G:"+this.g);
-        this.p = pIn.readLine();// llegada de P
+        this.p = pIn.readLine();// * llegada de P
         System.out.println("Esta es tu P: "+this.p);
 
-        this.gx = pIn.readLine();// llegada de G^x
+        this.gx = pIn.readLine();// * llegada de G^x
         System.out.println("Esta es tu gx: "+this.gx);
 
 
@@ -47,15 +53,30 @@ public class Client {
 
         String expected = this.g +","+this.p+"," +this.gx;// el mensaje correcto
         this.signature = pIn.readLine();//Firma digital
+        byte[] bsing = str2byte(this.signature);
         System.out.println("Esta es la firma electronica: "+ signature);
         // no tengo la llave publica 
-        //boolean ck =sc.checkSignature(, expected,this.signature,expected);
-        sc.checkSignature(publicaServer, this.signature, expected);
-        boolean ck = false;
+         try {
+            ck =sc.checkSignature(publicaServer, bsing,expected);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception("No se ha podido revisar la firma");// cuando no se puede hacer el check
+        }
         if (ck)
         {
-            pout.println("OK");// envío por le canal
-            pout.println();
+            pout.println("OK");//*  envío por le canal cuando está bien
+            BigInteger gB = new BigInteger(this.g);// g
+            BigInteger pB = new BigInteger(this.p);// p
+            SecureRandom r = new SecureRandom();// X
+            int x = Math.abs(r.nextInt());
+            Long longx = Long.valueOf(x);
+    		BigInteger Bx = BigInteger.valueOf(longx);
+
+
+            this.gy = G2Y(gB, Bx, pB);// generate Gy
+            pout.println(this.gx.toString());//  * Envio 6b Gy
+            
+
         }
         else
         {
@@ -86,7 +107,12 @@ public class Client {
             System.exit(-1);
         }
         BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-        cliente.procesar(stdIn, lector, escritor); 
+        try {
+			cliente.procesar(stdIn, lector, escritor);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
         stdIn.close();
         lector.close();
         escritor.close();
@@ -114,6 +140,12 @@ public class Client {
 		}
 		return ret;
 	}
+
+    public BigInteger G2Y (BigInteger base, BigInteger expoenent, BigInteger modulo)
+    {
+        return base.modPow(expoenent, modulo);
+
+    }
 
   
  
